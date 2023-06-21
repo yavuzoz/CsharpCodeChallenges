6b1be659da39d78844300b006dd1f26ac85c5734
Yavuz Özbay <ozbayavz@gmail.com>
2023-06-21 14:44:38 +0200

    reverse word issue is done

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool weiterGehen = false;
+
+            do
+            {
+                int eingabe;
+                Console.WriteLine("bitte zahl eingeben");
+                eingabe = int.Parse(Console.ReadLine());
+                if (unterprogram(eingabe))
+                {
+                    Console.WriteLine("schöne zahl ");
+                }
+                else { Console.WriteLine("kein schöne zahl "); }
+                Console.ReadLine();
+
+                Console.WriteLine("Möchten Sie weiter Zahl geben ? ( ja/nein)");
+                string antwort= Console.ReadLine();
+                if (antwort.ToLower() == "ja")
+                {
+                    weiterGehen=true;
+                }
+                
+
+            }while (weiterGehen==true);
+            Console.WriteLine("Programm beendet.");
+            Console.ReadLine();
+
+
+        }
+
+        public static bool unterprogram(int x)
+        {
+
+                bool resultat;
+                if (x % 2 == 0)
+                {
+                    resultat = true;
+                }
+                else
+                {
+                    resultat = false;
+
+                }
+     
+                return resultat;
+           
+        }
+    }
+}
---
+++ reverseName/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bitte Namen eingeben : ");
+            string name = Console.ReadLine();
+          
+            
+            string reversedName = reverseName(name);
+            Console.WriteLine(reversedName);
+            Console.ReadLine();
+        }
+
+        public static string reverseName(string name)
+        {
+            string rename="";
+            for(int i=name.Length-1; i>=0; i--)
+            {
+                rename += name[i]; 
+              
+            }
+            return rename;
+        }
+
+    }
+}
