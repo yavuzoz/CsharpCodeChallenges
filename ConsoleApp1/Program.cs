3cfe21ac3029e92d6e45b0aef7e7b6affdeda784
Yavuz Özbay <ozbayavz@gmail.com>
2023-06-23 21:19:39 +0200

    new file added

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace teufel-kreis
+{
+
+
+    class Program
+    {
+             static void Main(string[] args)
+             {
+
+                    Console.WriteLine("Geben Sie die Erdbebenstärke ein:");
+                     double erdbebenStaerke = Convert.ToDouble(Console.ReadLine());
+
+
+                    do
+                     {
+
+                       if (erdbebenStaerke > 5.0)
+                         {
+                    Console.WriteLine("Das ist Schicksal. Allah ist schuldig. Die Hause sind zerstört.");
+
+                         }
+                     else
+                     {
+                       Console.WriteLine("Wir sind Weltmarktführer! Das Haus bleibt stehen.");
+
+                      }
+
+                     }
+                  while (ignoranz && diktator = true);
+
+                 Console.ReadLine();
+              }
+        }
+}
---
+++ gerade-ungeradeZahl/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gerade_ungeradeZahl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("geben sie ein zahl1 ein : ");
+            int zahl1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("geben sie ein zahl2 ein : ");
+            int zahl2 = int.Parse(Console.ReadLine());
+
+            if (zahl1 > zahl2)
+            {
+                do
+                {
+                    if (zahl2 % 2 == 0)
+                    {
+                        Console.WriteLine(zahl2 + "gerade");
+                    }
+                    else
+                    {
+                        Console.WriteLine(zahl2 + " ungerade");
+                    }
+                    zahl2++;
+                }while (zahl2<=zahl1);
+            }
+            if (zahl2 > zahl1)
+            {
+                do
+                {
+                    if (zahl1 % 2 == 0)
+                    {
+                        Console.WriteLine(zahl1 + "gerade");
+                    }
+                    else
+                    {
+                        Console.WriteLine(zahl1 + " ungerade");
+                    }
+
+                    zahl1++;
+                } while (zahl1 <= zahl2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
