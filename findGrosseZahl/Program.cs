d04e3bdab8298203570c92fba13cdf40e028ece7
Yavuz Özbay <ozbayavz@gmail.com>
2023-06-23 21:33:53 +0200

    minNumber to find added

--- findGrosseZahl/Program.cs
+++ findGrosseZahl/Program.cs
@@ -11,22 +11,27 @@ namespace findGrosseZahl
         static void Main(string[] args)
         {
             Random random = new Random();
-            int[] zahlen = new int[10];
 
-            for(int i = 0; i < zahlen.Length; i++)
+            int[] zahlen = new int[10];
+            int maxNumber = zahlen[0];
+            int minNumber = 11;
+
+            for (int i = 0; i < zahlen.Length; i++)
             {
                 zahlen[i] = random.Next(0,11);
-            }
-
-            int maxNumber = zahlen[0];
-            for (int i = 0;i < zahlen.Length; i++)
-            {
+ 
                 if (zahlen[i] > maxNumber)
                 {
                     maxNumber = zahlen[i];
                 }
+                else if(zahlen[i] < minNumber)
+                {
+                   minNumber= zahlen[i] ;
+                }
             }
             Console.WriteLine("grosse zahl ist : " + maxNumber);
+            Console.WriteLine("kleiner zahl ist : " + minNumber);
+            
             Console.ReadLine();
 
         }
